82f000bc674e41926e4c468f25faefc06c8d0819
lumamy310 <lumamy310@gmail.com>
2022-06-22 16:00:45 -0400

    checkstyle fix

--- src/HangmanPanel.java
+++ src/HangmanPanel.java
@@ -1,7 +1,16 @@
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Frame;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.*;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 
 /**
